598e8ddaa56b4bb011e74b29451785254f94516a
Toru Sako <majikade.zou@gmail.com>
2019-12-06 19:50:00 +0900

    add integration rspec

---
+++ spec/systems/site_layout_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "SiteLayouts", type: :system do
+  describe "home layout" do
+    it "contains root links" do
+      visit root_path
+      expect(page).to have_link nil, href: root_path, count: 2
+    end
+
+    it "contains signup link" do
+      visit root_path
+      expect(page).to have_link 'はじめる', href: signup_path
+    end
+    it "contains login link" do
+      visit root_path
+      expect(page).to have_link 'login', href: login_path
+    end
+  end
+end
