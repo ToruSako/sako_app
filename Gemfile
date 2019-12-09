9de0c04e0d09b025586960b5e64139b146332109
Toru Sako <majikade.zou@gmail.com>
2019-12-09 09:30:00 +0900

    add show and signinpages

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '~> 6.0.1'
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem 'bcrypt'
+gem 'rails-i18n'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,18 @@
-# require 'rails_helper'
-#
-# RSpec.describe UsersController, type: :controller do
-#
-#   describe "GET #new" do
-#     it "returns http success" do
-#       get :new
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-#
-# end
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe "error message check" do
+      it "return can't be blank" do
+          user = User.new()
+          user.valid?
+          expect(user.errors.messages[:name]).to include("を入力してください")
+      end
+    end
+end
