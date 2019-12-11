f4336e1f532eae5d5a48dcf57325093b9520ecac
Toru Sako <majikade.zou@gmail.com>
2019-12-11 14:51:28 +0900

    specを確認

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -15,25 +15,63 @@ def post_valid_information(remember_me = 0)
     }
   }
 end
+  #間違った情報でログインするメソッド
+def post_invalid_information
+  post login_path, params: {
+    session: {
+      email: "",
+      password: ""
+    }
+  }
+end
 
-  it "sessions/newにアクセスできること" do
-    #ログインページにアクセスします
-    get login_path
-    expect(response).to have_http_status(:success)
-  end
 
-  describe "ログインページにアクセスした時" do
+  describe "ログインのテスト" do
+   context "無効な情報を与えた時" do
     it "無効なログイン情報を与えた場合、dangerフラッシュが出ること" do
-      post login_path
-      post login_path, params: {
-      session: {
-         email: "",
-         password: ""
-       }
-     }
-     expect(flash[:danger]).to be_truthy
-     expect(logged_in?).to be_falsey
+      get login_path
+      post_invalid_information
+      expect(flash[:danger]).to be_truthy
+      expect(is_logged_in?).to be_falsey
+    end
   end
+
+   context "有効な情報を与えた時" do
+   it "dangerフラッシュが出ないこと" do
+     get login_path
+     post_valid_information
+     expect(flash[:danger]).to be_falsey
+     expect(is_logged_in?).to be_truthy
+     follow_redirect!
+     expect(request.fullpath).to eq '/users/1'
+   end
+
+   it "ログアウトが成功すること" do
+     get login_path
+     post_valid_information
+     expect(is_logged_in?).to be_truthy
+     follow_redirect!
+     expect(request.fullpath).to eq '/users/1'
+     delete logout_path
+     expect(is_logged_in?).to be_falsey
+     follow_redirect!
+     expect(request.fullpath).to eq '/'
+   end
+
+   it "2度ログアウトしないこと" do
+     get login_path
+     post_valid_information
+     expect(is_logged_in?).to be_truthy
+     follow_redirect!
+     expect(request.fullpath).to eq '/users/1'
+     delete logout_path
+     expect(is_logged_in?).to be_falsey
+     follow_redirect!
+     expect(request.fullpath).to eq '/'
+     delete logout_path
+     follow_redirect!
+     expect(request.fullpath).to eq '/'
+   end
 
    it "チェックボックスをオンにしたことでリメンバートークンが保存されること" do
      get login_path
@@ -48,32 +86,6 @@ end
     expect(is_logged_in?).to be_truthy
     expect(cookies[:remember_token]).to be_nil
    end
-
-
-
-   it "有効なログイン情報を与えた場合、dangerフラッシュが出ないこと" do
-     get login_path
-     post login_path, params: {
-       session: {
-         email: user.email,
-         password: user.password
-       }
-     }
-     expect(flash[:danger]).to be_falsey
-     expect(logged_in?).to be_truthy
-   end
-   it "succeeds login and logout" do
-        get login_path
-        post login_path, params: {
-          session: {
-            email: user.email,
-            password: user.password
-          }
-        }
-        expect(is_logged_in?).to be_truthy
-        delete logout_path
-        expect(is_logged_in?).to be_falsey
-   end
  end
-
 end
+end 
